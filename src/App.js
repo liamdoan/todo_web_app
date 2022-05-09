646b56da7910cff0e085f2214045bedd36d341e2
liamdoan <lamdoan9898@gmail.com>
2022-05-10 01:15:31 +0300

    2ndt commit

--- src/App.js
+++ src/App.js
@@ -4,97 +4,129 @@ import './App.css';
 
 const App = () => {
 
-  const [todos, setTodos] = useState([])
+  let time = String(new window.Date())     
+    let date = time.slice(0,25)
 
-  // keep track of current todo
-  const [todo, setTodo] = useState("")
+    const [todos, setTodos] = useState([])
 
-  const [todoEditing, setTodoEditing] = useState(null)
+    // keep track of current todo
+    const [todo, setTodo] = useState("")
+    const [desc, setDesc] = useState("")
 
-  const [editingText, setEditingText] = useState("")
+    const [todoEditing, setTodoEditing] = useState(null)
 
- 
+    const [editingText, setEditingText] = useState("")
+    const [editingDesc, setEditingDesc] = useState("")
 
-  function handleSubmit(e) {
-    // need e value, because by default form will refresh the page when submitted
-    e.preventDefault()
+  
+// ----------------------------SUBMIT-------------------------
+    function handleSubmit(e) {
+      e.preventDefault()
 
-    const newTodo = {
-      id: new Date().getTime(),
-      // better to have id when working with multiple object
-      text: todo,
-      completed: false
+      const newTodo = {
+        id: new Date().getTime(),
+        // better to have id when working with multiple object
+        timeCreate: "Created at: " + date,
+        timeUpdate: "",
+        text: todo,
+        description: desc,
+        completed: false
+      }
+      setTodos([...todos].concat(newTodo))
+      // reset input after adding newTodo, by setting todo to an empty string
+      setTodo("")
+      setDesc("")
     }
- 
-    setTodos([...todos].concat(newTodo))
-
-    // reset input after adding newTodo, by setting todo to an empty string
-    setTodo("")
-  }
 
 
-
-  function toggleComplete(id) {
-    const updatedTodos = [...todos].map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-
-    setTodos(updatedTodos)
-  }
-
-
-  function editTodo(id) {
-    const updatedTodos = [...todos].map(todo => {
-      if(todo.id === id ) {
-        todo.text = editingText
-      }
-      return todo
-    })
-    setTodos(updatedTodos)
-
-     // reset
-     setTodoEditing(null)
-     setEditingText("")
-  }
+  // ------------------------------TOGGLE COMPLETE--------------------------
+    function toggleComplete(id) {
+      const updatedTodos = [...todos].map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      setTodos(updatedTodos)
+    }
 
 
-  function deleteTodo(id) {
-    const updatedTodos = [...todos].filter(todo => todo.id !== id)
-    setTodos(updatedTodos)
-  }
+  // ------------------------------EDIT TODO-----------------------------
+    function editTodo(id) {
+      const updatedTodos = [...todos].map(todo => {
+        if(todo.id === id ) {
+          todo.text = editingText
+          todo.description = editingDesc
+          todo.timeUpdate = "Update at: " + date
+        }
+        return todo
+      })
+      setTodos(updatedTodos)
+      // reset
+      setTodoEditing(null)
+      setEditingText("")
+      setEditingDesc("")
+    }
 
+
+  // ------------------------DELETE--------------------------------
+    function deleteTodo(id) {
+      const updatedTodos = [...todos].filter(todo => todo.id !== id)
+      setTodos(updatedTodos)
+    }
 
   return (
     <div className="wrapper">
       <h1>Tasks for today</h1>
 
-      {/*FORM*/}
+{/*-----------FORM---------------*/}
       <form className="todo-form" onSubmit={handleSubmit}>
-          {/* handleSubmit will run everytime the button is clicked */}
-        <input type="text" 
-                placeholder="What to do"
-                onChange={(e) => setTodo(e.target.value)}
-                value={todo}
-                className="todo-input"/>
+        <div className="input-column">
+          <input type="text" 
+                  placeholder="What to do"
+                  onChange={(e) => setTodo(e.target.value)}
+                  value={todo}
+                  className="todo-input"/>
+          <input type="text" 
+                  placeholder="Description"
+                  onChange={(e) => setDesc(e.target.value)}
+                  value={desc}
+                  className="desc-input"/>
+        </div>
         <button className="submit-button" 
                 type="submit"
                 >
                   Add Tasks
-                </button>
+        </button>
       </form>
 
-      {/* LIST */}
+{/*----------------------LIST-------------------------*/}
       {todos.map(todo => 
           <div className="todo-row" 
                 key={todo.id}>
-            {todoEditing === todo.id 
-            ?   <input type="text" 
+            {
+              todoEditing === todo.id 
+              ? 
+              <div className="input-edit-wrap">  
+                <input type="text" 
                       onChange={(e) => setEditingText(e.target.value)} 
                       value={editingText}/>
-            : <div>{todo.text}</div>}
+                <input type="text" 
+                      onChange={(e) => setEditingDesc(e.target.value)} 
+                      value={editingDesc}/>
+              </div>
+              : 
+              <div className="input-show">
+                <p className="input-show-name">{todo.text}</p>
+                <p className="input-show-desc">{todo.description}</p>
+                <p className="input-show-time">
+                  <span className="span-time">{todo.timeCreate}</span>
+                </p>
+                <p className="input-show-time">
+                  <span className="span-time">{todo.timeUpdate}</span>
+                </p>
+              </div>
+            }
               {/* 'todo' is changable */}
              
               <div className="buttons">
@@ -102,8 +134,8 @@ const App = () => {
                         onChange={() => toggleComplete(todo.id)}
                         checked={todo.completed}
                         className="check-complete"/>
-
-                {todoEditing === todo.id 
+                {
+                  todoEditing === todo.id 
                   ? <button className="submit-edit-btn" 
                             onClick={() => editTodo(todo.id)}>
                       Submit Edit
@@ -111,9 +143,9 @@ const App = () => {
                   : <button className="edit-btn" 
                             onClick={() => setTodoEditing(todo.id)}>
                       Edit Tasks
-                    </button>}
-
-                <button className="delete-btn" 
+                    </button>
+                }
+                  <button className="delete-btn" 
                         onClick={() => deleteTodo(todo.id)}>
                     Delete
                   </button>
@@ -122,7 +154,7 @@ const App = () => {
             </div>
             )}
     </div>
-  );
+  )
 }
 
 export default App;
